98c24e640739b25908bf21f47ae75cf250e9a7ba
Clint Losee <clint.losee@gmail.com>
2025-04-09 11:59:31 -0600

    Updated prettier and eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
-import prettier from "eslint-plugin-prettier";
-import configPrettier from "eslint-config-prettier";
+import withNuxt from './.nuxt/eslint.config.mjs';
+import prettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
 
 export default withNuxt(
   {
     plugins: { prettier },
     rules: {
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      quotes: ['error', 'single', { avoidEscape: true }], // Enforce single quotes
     },
   },
   configPrettier,
