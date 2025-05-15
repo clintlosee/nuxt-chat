e2d931b10a2e5448f2e12b56a79aa4dba054a8ad
Clint Losee <clint.losee@gmail.com>
2025-05-15 15:29:18 -0600

    Layers setup and refactor

--- layers/base/app/layouts/default.vue
+++ layers/base/app/layouts/default.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import AppHeader from '~/components/AppHeader.vue';
 const isSidebarOpen = ref(true);
 </script>
 
---
+++ layers/base/nuxt.config.ts
@@ -0,0 +1,18 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  css: ['./layers/base/app/assets/css/main.css'],
+
+  modules: ['@nuxt/ui', '@nuxtjs/mdc'],
+
+  mdc: {
+    highlight: {
+      theme: 'github-light',
+      langs: ['html', 'markdown', 'javascript', 'typescript', 'vue'],
+    },
+  },
+});
