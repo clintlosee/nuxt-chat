c63241ae92b22eefb6ec26cec3c6d95d81b7bb2c
Clint Losee <clint.losee@gmail.com>
2025-08-17 08:57:58 -0600

    Cache setup for getAllChats and invalidate cache on new chat creation.

--- layers/chat/server/api/chats/index.get.ts
+++ layers/chat/server/api/chats/index.get.ts
@@ -1,5 +1,19 @@
 import { getAllChats } from '../../repository/chatRepository';
 
-export default defineEventHandler(async (_event) => {
-  return getAllChats();
-});
+export default defineCachedEventHandler(
+  async (_event) => {
+    const storage = useStorage('db');
+    await storage.setItem('chats:has-new-chat', false);
+    return getAllChats();
+  },
+  {
+    name: 'getAllChats',
+    maxAge: 0, // 0 disables caching until the cache is invalidated
+    swr: false, // Stale-while-revalidate
+    async shouldInvalidateCache() {
+      const storage = useStorage('db');
+      const hasNewChat = await storage.getItem<boolean>('chats:has-new-chat');
+      return hasNewChat || false;
+    },
+  },
+);
